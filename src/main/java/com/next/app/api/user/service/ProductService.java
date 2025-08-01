7f35d520c332c3e71758728b0c59adaac2dc120e
gogogojun <rhwnsgh11@naver.com>
2025-08-01 16:01:44 +0900

    business api

--- src/main/java/com/next/app/api/user/service/ProductService.java
+++ src/main/java/com/next/app/api/user/service/ProductService.java
@@ -3,7 +3,6 @@ package com.next.app.api.user.service;
 
 import com.next.app.api.user.entity.Product;
 import com.next.app.api.user.repository.ProductRepository;
-import com.next.app.api.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
